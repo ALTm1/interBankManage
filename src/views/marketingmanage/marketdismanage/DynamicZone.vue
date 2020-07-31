ac8650454562d1c3a503ac665fade7ddc4b3f583
xushuai <1720285317@qq.com>
2020-07-31 09:30:05 +0800

    2020/7/31-3s

--- src/views/marketingmanage/marketdismanage/DynamicZone.vue
+++ src/views/marketingmanage/marketdismanage/DynamicZone.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-07-30 19:52:15
- * @LastEditTime: 2020-07-30 20:00:41
+ * @LastEditTime: 2020-07-31 09:29:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \interBankManage\src\views\marketingmanage\marketdismanage\DynamicZone.vue
@@ -13,6 +13,23 @@
       <DisplayItem :title="item.title" :content="item.content"></DisplayItem>
       <span class="choose-text">选择</span>
     </div>
+    <!-- <ui-dialog title="会议列表" center :visible.sync="meetingDialog">
+      <ui-table :data="tableData" v-if="showTable">
+        <ui-table-column prop="order" label="序号" align="center" min-width="100px"></ui-table-column>
+        <ui-table-column prop="proLevelOne" label="产品一级分类" align="center" min-width="100px"></ui-table-column>
+        <ui-table-column prop="proLevelTwo" label="产品二级分类" align="center" min-width="100px"></ui-table-column>
+        <ui-table-column prop="tradeDirec" label="交易方向" align="center" min-width="100px"></ui-table-column>
+        <ui-table-column prop="proName" label="产品名称" align="center" min-width="100px"></ui-table-column>
+        <ui-table-column prop="organName" label="机构全称" align="center" min-width="100px"></ui-table-column>
+        <ui-table-column prop="proRate" label="利率" align="center" min-width="100px"></ui-table-column>
+        <ui-table-column prop="timeLimit" label="期限" align="center" min-width="100px"></ui-table-column>
+        <ui-table-column prop="money" label="金额" align="center" min-width="100px"></ui-table-column>
+      </ui-table>
+      <div class="footer">
+        <span class="cancel-button float-left" @click="meetingDialog=!meetingDialog">取消</span>
+        <span class="confirm-button float-right" @click="meetingDialog=!meetingDialog">确认</span>
+      </div>
+    </ui-dialog>-->
     <div style="text-align: center">
       <Button text="提交" backgroundColor="#9B7041"></Button>
     </div>
@@ -35,6 +52,15 @@ export default {
         {
           title: '推送机构动态',
           content: '机构动态标题1',
+        },
+      ],
+      // 会议列表弹框
+      meetingDialog: false,
+      meetingTable: [
+        {
+          order: 1,
+          meetingTitle: '江南银行会议',
+          releaseDate: '2020-09-09',
         },
       ],
     }
